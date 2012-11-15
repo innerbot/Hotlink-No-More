182fa1b89bc0e292ddd5e569584e09f861dcfe52
Greg Johnson <greg@innerbot.com>
2012-11-14 20:26:08 -0600

    Stubbed out the control flow and logic for the plugin.

--- index.php
+++ index.php
@@ -30,3 +30,112 @@ Author URI: http://innerbot.com
  * **********************************************************************
  */
 
+class HotlinkNoMore {
+
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	function __construct() {
+
+		// load plugin text domain
+		add_action( 'init', array( $this, 'textdomain' ) );
+
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+	    $this->register_hotlink_scanner();
+	
+	} // end constructor
+
+	/**
+	 * Fired when the plugin is activated.
+	 */
+	public function activate() {
+		// TODO define activation functionality here
+	} // end activate
+
+	/**
+	 * Fired when the plugin is uninstalled.
+	 */
+	public function uninstall() {
+		delete_option('hotlink-no-more');
+	} // end uninstall
+
+	/**
+	 * Loads the plugin text domain for translation
+	 */
+	public function textdomain() {
+		
+		load_plugin_textdomain( 'hotlink-no-more', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+	}
+
+	// helper function to hook up the hotlink_scanner during 
+	// instantiation and at the end of HotlinkNoMore::localize_hotlinked_files()
+	function register_hotlink_scanner() {
+    	add_action( 'save_post', array( &$this, 'localize_hotlinked_files' ) );
+	} // end register_hotlink_scanner()
+
+	// helper function to unhook the hotlink scanner
+	// used to prevent infinite loop when save_post hook fires
+	function unhook_hotlink_scanner() {
+		remove_action( 'save_post', array( &$this, 'localize_hotlinked_files' ) );
+	} // end unhook_hotlink_scanner()
+
+	
+
+	function localize_hotlinked_files($post_id) {
+
+		if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
+			return;
+
+		$the_content = $_POST['content'];
+
+		$links = $this->extract_hotlinks($the_content);
+
+		foreach( $links as $link ) {
+
+			// check that link matches file extensions
+			if($this->allowed_extensions($link)) {
+				// download the file
+				if( $local_file_path = $this->make_it_local($link, $post_id) )
+					// replace external url with new local url
+					str_replace($link, $local_file_path, $the_content);
+			}
+		}
+
+		$this->unhook_hotlink_scanner();
+
+		wp_update_post( array( 'ID'=>$post_id, 'post_content' => $the_content ) );
+
+		$this->register_hotlink_scanner();
+
+	} // end localize_hotlinked_files()
+
+	// returns an array of all url's in $content
+	function extract_hotlinks($content) {
+
+
+
+	} // end extract_hotlinks()
+
+	// breaks link down to see if the filename of the url is
+	// of an allowed filetype
+	function allowed_extensions($url) {
+
+	} // end allowed_extensions()
+
+	// This takes the provided url, downloads it to the server
+	// inserts the file into the media db as an attachment, and 
+	// returns the new local url
+	function make_it_local($url, $post_id) {
+
+	} // end make_it_local()
+	  
+} // end class
+
+
+$hotlink_no_more = new HotlinkNoMore();
