5339d5f9c6ee97fb488aeddd996b060ca981c736
Greg Johnson <greg@innerbot.com>
2012-11-14 18:47:02 -0600

    Added plugin stub file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: HotLink no More
+Plugin URI: http://innerbot.com/wordpress-plugins/hotlink-no-more
+Description: A simple plugin that automagically processes all posts and pages that hotlink to images, downloading the images to your uploads folder, and replacing hotlinks with local links.
+Version: 0.1
+Author: Greg Johnson
+Author URI: http://innerbot.com 
+*/
+
+/**
+ * Copyright 2012, InnerBot.com. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
